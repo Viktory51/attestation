c49a53773e43c891d68bb7856adbfe1d956a865f
viktoriakazanskaya <vikamur_555@mail.ru>
2024-03-30 01:22:47 +0400

    решение

---
+++ zadacha3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы
+
+void MassivPh(int [] arr, int i = 0)
+{
+    if (i == arr.Length)
+{ return; }
+MassivPh(arr, i + 1);
+Console.Write($"{arr[i]} ");
+}
+
+
+int[] arr = { 1, 2, 5, 0, 10, 34 };
+MassivPh(arr);
